37301e76194a2629d583357a337f26c2e8ac6b8a
Théodore Biadala <mail@theodoreb.net>
2010-10-23 23:42:33 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+User Pain for Redmine
+======================
+
+TODO
---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_redmine_user_pain do
+  name 'Redmine Redmine User Pain plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
