3560d703b9605e1a5dc74a6a4831c3201bd310be
Théodore Biadala <mail@theodoreb.net>
2010-10-24 15:37:28 +0200

    Way nicer code thanks to Effort-Estimate plugin source :)

--- lib/redmine_user_pain/issue_patch.rb
+++ lib/redmine_user_pain/issue_patch.rb
@@ -2,64 +2,37 @@ module RedmineUserPain
   # Patches Redmine's Issues dynamically.  Adds a +after_save+ filter.
   module IssuePatch
     def self.included(base) # :nodoc:
-      base.extend(ClassMethods)
-
       base.send(:include, InstanceMethods)
-
-      # Same as typing in the class 
       base.class_eval do
         unloadable # Send unloadable so it will not be unloaded in development
         before_save :update_user_pain_from_issue
       end
-
-    end
-    
-    module ClassMethods
     end
     
     module InstanceMethods
-      # This will update the KanbanIssues associated to the issue
+      # calculate user pain from tracker, priority and custom Likelihood values
       def update_user_pain_from_issue
+        # get the last custom field
+        likelihood = CustomField.find_by_name("Likelihood")
+        likelihood_values_length = likelihood['possible_values'].length
         pain = 1
-        pain_type = 1
-        pain_priority = 1
-        pain_likelihood = 1
-        # taille liste tracker
-        trackers = Tracker.all.length
-        # taille liste priorité
-        priorities = IssuePriority.find(:all).length
-        # taille liste audience
-        audience = self.available_custom_fields[0][:possible_values].length
 
-        max_pain = trackers * priorities * audience
+        # add likelihood value to total pain
+        self.custom_values.each do |x|
+          if x.custom_field_id == likelihood.id
+            pain *= likelihood_values_length - likelihood['possible_values'].index(x.value)
+          end
+        end
 
-        # index du tracker
-        Tracker.all.each { |t|
-          if t.id == self.tracker_id
-            pain_type = (trackers - (t.position-1))
-          end
-        }
-        # index de la priorité
-        IssuePriority.find(:all).each { |p|
-          if p.id == self.priority_id
-            pain_priority = (priorities - (p.position-1))
-          end
-        }
-        # index de l'audience
-        pain_likelihood = self.custom_field_values[0]['value']
-        pain_likelihood = self.available_custom_fields[0][:possible_values].index(pain_likelihood)
-        pain_likelihood = audience - pain_likelihood
-        
-        # index de l'audience 
-        pain = pain_type * pain_priority * pain_likelihood
+        # add type value to total pain
+        pain *= Tracker.all.length - (Tracker.find_by_id(self.tracker_id).position - 1)
+        pain *= IssuePriority.all.length - (IssuePriority.find_by_id(self.priority_id).position - 1)
+
+        max_pain = Tracker.all.length * IssuePriority.all.length * likelihood_values_length
         user_pain = 100 * pain / max_pain
-
         self.custom_field_values[1][:value] = (user_pain).to_s
-        #total * user_pain / 100
-         
         return true
       end
-
     end    
   end
 end
